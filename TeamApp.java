46c477153d71ef07f4480d7d5c56c467c425738d
POTHURU-JASWANTH <pothuru.jaswanth2023@vitstudent.ac.in>
2026-01-08 16:43:12 +0530

    Added contribution by Member 1

--- TeamApp.java
+++ TeamApp.java
@@ -1,5 +1,7 @@
 public class TeamApp {
 public static void main(String[] args) {
 System.out.println("Team Collaboration Application");
+System.out.println("Contribution by Member 1 (Jaswanth P)");
+
 }
 }
