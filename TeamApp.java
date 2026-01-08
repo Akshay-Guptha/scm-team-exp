c81207c643add3eac9b5df6f2e8d73cc32ab12bc
DhanushReddy1717 <dhanushreddy1717@gmail.com>
2026-01-08 17:28:54 +0530

    Added contribution by Member 2

--- TeamApp.java
+++ TeamApp.java
@@ -2,6 +2,7 @@ public class TeamApp {
 public static void main(String[] args) {
 System.out.println("Team Collaboration Application");
 System.out.println("Contribution by Member 1 (Jaswanth P)");
+System.out.println("Contribution by Member 2 (Y Dhanush )");
 
 }
 }
